44f45f60e045ca3fb45725c6b3375cbb76aba8c0
PC-KESCH\KESCH <golinveau-kesch@oulook.com>
2021-04-21 14:41:19 +0200

    DK Mode & Copy

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,18 @@
 "use strict";
+//let lightmodeBtn = document.querySelector('.lightmode');
+//let activeMode = localStorage.getItem("mode");
+//if(activeMode == "dark"){
+//    document.body.classList.add("dark");
+//}
+//lightmodeBtn.addEventListener("click", (e) =>{
+//    document.body.classList.toggle("dark");
+//    if(document.body.classList.contains("dark")){
+//        localStorage.setItem("mode", "dark");
+//    }else{
+//        localStorage.setItem("mode", "light");
+//    } 
+//});
+//let ajd = new Date();
+//let year = ajd.getFullYear();
+//let copyright = document.querySelector(".copyright");
+//copyright.innerHTML = "©"+ year
