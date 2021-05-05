149b312ba4cb3cf16646370f5dbc9f6a0729ba58
PC-KESCH\KESCH <golinveau-kesch@oulook.com>
2021-05-05 20:19:25 +0200

    AVCMT 05/05

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -12,6 +12,32 @@ lightmodeBtn.addEventListener("click", (e) =>{
         localStorage.setItem("mode", "light");
     } 
 });
+
+function Pages(){
+	let pages = document.querySelectorAll(".page--active");
+	for(let page of pages){
+		page.classList.remove("page--active");
+	}
+}
+let menuButtons = document.querySelectorAll(".menu__btn");
+let navLinks = document.querySelectorAll(".nav__el a");
+for(let menuButton of menuButtons){
+	menuButton.addEventListener("click", (e) => {
+		document.body.classList.toggle("body--nav-open");
+	});
+}
+for(let navLink of navLinks){
+	navLink.addEventListener("click", (e) => {
+		Pages();
+		let PageIdActif = e.currentTarget.getAttribute("href");
+		let PageActive = document.querySelector(PageIdActif);
+		if(PageActive){
+			PageActive.classList.add("page--active");
+		}
+		document.body.classList.remove("body--nav-open");
+	});
+}
+
 let ajd = new Date();
 let year = ajd.getFullYear();
 let copyright = document.querySelector(".liste-compl__el--copyright");
